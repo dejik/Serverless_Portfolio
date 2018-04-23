2349971e8644796c6cbee81bdfe207f51e02228a
Dejik Hyde <dejik.hyde@coxautoinc.com>
2018-04-23 09:57:51 -0400

    mimtype

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.client import Config
 import  StringIO
 import zipfile
+import mimetypes
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 portfolio_bucket = s3.Bucket('dejikportfolio')
 build_bucket = s3.Bucket('portfoliobuild.dejikportfoli')
@@ -11,4 +12,5 @@ with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj, nm)
+            ExtraArgs=('ContentType': mimetypes.guess_type()[0])
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
